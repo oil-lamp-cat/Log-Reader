f10d371b838c146a38fc9d9454043262713dcbed
oil-lamp-cat <103806022+oil-lamp-cat@users.noreply.github.com>
2024-06-11 17:39:26 +0900

    Update MainLog.sh
    done?

--- MainLog.sh
+++ MainLog.sh
@@ -2,12 +2,12 @@
 echo "
 
 
-██████╗ ██████╗     ██╗      ██████╗  ██████╗ 
-██╔══██╗██╔══██╗    ██║     ██╔═══██╗██╔════╝ 
-██████╔╝██║  ██║    ██║     ██║   ██║██║  ███╗
-██╔══██╗██║  ██║    ██║     ██║   ██║██║   ██║
-██║  ██║██████╔╝    ███████╗╚██████╔╝╚██████╔╝
-╚═╝  ╚═╝╚═════╝     ╚══════╝ ╚═════╝  ╚═════╝ 
+		██████╗ ██████╗     ██╗      ██████╗  ██████╗ 
+		██╔══██╗██╔══██╗    ██║     ██╔═══██╗██╔════╝ 
+		██████╔╝██║  ██║    ██║     ██║   ██║██║  ███╗
+		██╔══██╗██║  ██║    ██║     ██║   ██║██║   ██║
+		██║  ██║██████╔╝    ███████╗╚██████╔╝╚██████╔╝
+		╚═╝  ╚═╝╚═════╝     ╚══════╝ ╚═════╝  ╚═════╝ 
 
 "
 
@@ -26,6 +26,12 @@ PREVIOUS_DOWN=0
 
 #UP 수
 CURRENT_DOWN=0
+
+#DOWN 출력 초기화
+DOWN_LOG=""
+
+#DOWN 수 초기화
+DOWN_COUNT=0
 
 # 알람 출력
 alarm() {
@@ -67,19 +73,27 @@ check_file_change() {
 		if [ "$CURRENT_DATE" != "$PREVIOUS_DATE" ]; then
 
 			#UP 수 확인
-			echo "UP 수 : `cat $LOG_FILE | grep -c "UP"`"
+			echo " UP 수 : `cat $LOG_FILE | grep -c "UP"`"
 
 			#DOWN 수 확인
 			CURRENT_DOWN=$(grep -c "DOWN" "$LOG_FILE")
-			echo "DOWN 수 : $CURRENT_DOWN"
+			echo " DOWN 수 : $CURRENT_DOWN"
 
 			#CRITICAL 이 존재할 때
 			if grep -q "CRITICAL" "$LOG_FILE"; then
-				echo "CRITICAL 수 : `cat $LOG_FILE | grep -c "CRITICAL"`"
+				echo " CRITICAL 수 : `cat $LOG_FILE | grep -c "CRITICAL"`"
 			fi
 
 			#DOWN수가 변경되었을 때 소리내기
 			if [ $CURRENT_DOWN -gt $PREVIOUS_DOWN ]; then
+				echo  -e "\n DOWN 발생"
+				DOWN_LOG=$(grep "DOWN" "$LOG_FILE")
+				DOWN_COUNT=$(($CURRENT_DOWN - $PREVIOUS_DOWN ))
+				echo " $PREVIOUS_DOWN -> $CURRENT_DOWN"
+				echo -e "\n========= LOG =========\n"
+				echo -e "\n$DOWN_LOG" | tail -n $DOWN_COUNT
+				echo -e "\n======================="
+				echo -e "\n알람을 종료하기 위해 q를 누르세요"
 				sound_alarm
 			fi
 
